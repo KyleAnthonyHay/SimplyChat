9bf181e2bfade4f9ccd48c5215ffcdc184f3e4c5
KyleAnthonyHay <haykyle917@gmail.com>
2024-05-24 05:41:12 -0400

    Initial Commit

---
+++ SimplyChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimplyChat
+//
+//  Created by Kyle-Anthony Hay on 5/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimplyChat/SimplyChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplyChatApp.swift
+//  SimplyChat
+//
+//  Created by Kyle-Anthony Hay on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SimplyChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
